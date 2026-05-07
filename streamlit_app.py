ce9fdbac36729472874a9ecaf62c88aa380deb7f
tcoyle1433 <tcoyle1433@outlook.com>
2026-05-07 10:49:03 -0500

    Fix import statement and format URL in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,6 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-    import requests  
-    smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+    import requests
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
     st.text(smoothiefroot_response)
